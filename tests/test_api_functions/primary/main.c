eefe6d069f2b17beca43b0e0c35e7de633a272a1
Michal Princ <michal.princ@nxp.com>
2025-02-03 11:15:09 +0100

    [RPSDK-1339] multicore_tests Enable test build
    This commit adds new CMake and Kconfig build system for multicore unit
    tests.
    
    Squashed commit of the following:
    
    commit 56c3530d580130295b2c774594fa9d824b055b89
    Author: Michal Princ <michal.princ@nxp.com>
    Date:   Mon Feb 3 10:22:14 2025 +0100
    
        [RPSDK-1339] Update copyright
    
        Signed-off-by: Michal Princ <michal.princ@nxp.com>
    
    commit 3f99102b02df37682099c7262c702097cf8bd472
    Author: Michal Princ <michal.princ@nxp.com>
    Date:   Wed Jan 15 09:31:11 2025 +0100
    
        [RPSDK-1339] Allow K32L3A60 MCMGR unit testing using the flash target for the secondary core
    
        Signed-off-by: Michal Princ <michal.princ@nxp.com>
    
    commit 26026c437a53b79f9411f4dca693a6d8a5d626c3
    Author: Michal Princ <michal.princ@nxp.com>
    Date:   Tue Jan 14 20:30:22 2025 +0100
    
        [RPSDK-1339] Support missing frdmk32l3a6, kw47evk, kw47loc boards
    
        Signed-off-by: Michal Princ <michal.princ@nxp.com>
    
    commit ed024babb7f2a3fe8918286d17b575299a122acc
    Author: Michal Princ <michal.princ@nxp.com>
    Date:   Tue Jan 14 16:29:13 2025 +0100
    
        [RPSDK-1339] Support KW47 and MCXW72 MCMGR unit testing
    
        Signed-off-by: Michal Princ <michal.princ@nxp.com>
    
    commit 6a50c2d01f2df7b006e3346bb3635ca8317cdeb6
    Author: Michal Princ <michal.princ@nxp.com>
    Date:   Mon Jan 13 16:18:43 2025 +0100
    
        [RPSDK-1339] Enable MCMGR unit testing in MCUXpressoSDK, allow board specific settings
    
        Signed-off-by: Michal Princ <michal.princ@nxp.com>
    
    commit 8ab71e4d009266ef8b635274bd1bcab6613fee82
    Author: Michal Princ <michal.princ@nxp.com>
    Date:   Fri Dec 6 23:24:41 2024 +0100
    
        [RPSDK-1339] Enable MCMGR unit testing in MCUXpressoSDK
    
        - This commit adds new CMake and Kconfig build system for MCMGR unit tests.
    
        Signed-off-by: Michal Princ <michal.princ@nxp.com>

---
+++ tests/test_api_functions/primary/main.c
@@ -0,0 +1,377 @@
+/*
+ * Copyright 2016-2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <string.h>
+#include "board.h"
+#include "mcmgr.h"
+#include "unity.h"
+#include <stdio.h>
+#include "app.h"
+#include "fsl_debug_console.h"
+
+// Libraries necessary to work with FW ELE, defined by previous macros.
+#if (defined(MIMXRT1187_cm33_SERIES) || defined(MIMXRT1189_cm33_SERIES))
+#include "ele_crypto.h" /* ELE Crypto SW */
+#include "fsl_s3mu.h"   /* Messaging unit driver */
+#endif
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+// This macros are defined for testing mcmgr API functions
+
+// number of cores in the system, this value should return API function
+// MCMGR_GetCoreCount()
+#if defined(MIMXRT798S_cm33_core0_SERIES)
+#define CORE_COUNT 4
+#else
+#define CORE_COUNT 2
+#endif
+
+// invalid number of core, used as invalid argument to API functions
+#define INVALID_CORE_NUMBER 100
+
+// address of RAM, which is used to tests for read/write from both cores
+// this address should be from memory region defined by previous macros
+#if defined(KW45B41Z83_cm33_SERIES)
+#define TEST_ADDRESS (((uint32_t)0x489C0000))
+#elif (defined(KW47B42ZB7_cm33_core0_SERIES) || defined(MCXW727C_cm33_core0_SERIES))
+#define TEST_ADDRESS (((uint32_t)0x489C8800))
+#elif defined(K32L3A60_cm4_SERIES)
+#define TEST_ADDRESS (((uint32_t)0x2002E800))
+#elif (defined(MIMXRT1187_cm33_SERIES) || defined(MIMXRT1189_cm33_SERIES))
+#define S3MU         MU_RT_S3MUA
+#define TEST_ADDRESS (((uint32_t)0x20520000))
+#elif (defined(MIMXRT798S_cm33_core0_SERIES))
+#define TEST_ADDRESS            (((uint32_t)0x20200000))
+#define CPU1_RAM_ATBITTER0_ADDR (0x40041044)
+#else
+#define TEST_ADDRESS (((uint32_t)CORE1_BOOT_ADDRESS) + 0x100)
+#endif
+#define TEST_VALUE 0xAAAAAAAA
+
+// RAM address from which boot secondary core
+#define BOOT_ADDRESS (void *)(char *)CORE1_BOOT_ADDRESS
+
+// value which should return API function MCMGR_GetCurrentCore() for this core
+#define THIS_CORE kMCMGR_Core0
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+#if (defined(KW45B41Z83_cm33_SERIES) || defined(KW47B42ZB7_cm33_core0_SERIES) || defined(MCXW727C_cm33_core0_SERIES) || defined(K32L3A60_cm4_SERIES))
+/* secondary core code runs always from flash, put the minimalistic image into the secondary core flash */
+#if defined(__ICCARM__) /* IAR Workbench */
+#pragma location = "__core1_image"
+__root const uint32_t code[]
+#elif defined(__GNUC__)
+const uint32_t code[] __attribute__((section(".core1_code")))
+#endif
+    = {
+#if defined(KW45B41Z83_cm33_SERIES)
+        (uint32_t)0x0014C000, // SP
+        (uint32_t)0x9,        // PC
+        0x49024801,           // first two instructions
+        0xe7fe6001,           // next two instructions
+        0xB0000000,           // test address where second core writes (TEST_ADDRESS & 0x0000FFFFu) + 0xB0000000u)
+        TEST_VALUE            // value second core writes
+#elif (defined(KW47B42ZB7_cm33_core0_SERIES) || defined(MCXW727C_cm33_core0_SERIES))
+        (uint32_t)0x00154000, // SP
+        (uint32_t)0x9,        // PC
+        0x49024801,           // first two instructions
+        0xe7fe6001,           // next two instructions
+        0xB0008800,           // test address where second core writes (TEST_ADDRESS & 0x0000FFFFu) + 0xB0000000u)
+        TEST_VALUE            // value second core writes
+#elif defined(K32L3A60_cm4_SERIES)
+        (uint32_t)0x09020000, // SP
+        (uint32_t)0x9,        // PC
+        0x49034802,           // first two instructions
+        0xe7fe6001,           // next two instructions
+        0xbf00bf00,           // Filler of NOPs to keep aligment
+        TEST_ADDRESS,         // test address where second core writes
+        TEST_VALUE            // value second core writes
+#endif
+   
+ };
+#endif
+
+// Test of MCMGR_Init() API function
+void mcmgr_test_init_success()
+{
+    /* Initialize MCMGR - low level multicore management library.
+       Call this function as close to the reset entry as possible,
+       (into the startup sequence) to allow CoreUp event trigerring. */
+    mcmgr_status_t retVal = kStatus_MCMGR_Error;
+#if (defined(KW45B41Z83_cm33_SERIES) || defined(KW47B42ZB7_cm33_core0_SERIES) || defined(MCXW727C_cm33_core0_SERIES))
+    retVal = MCMGR_EarlyInit();
+    TEST_ASSERT(retVal == kStatus_MCMGR_Success);
+#endif
+
+    retVal = MCMGR_Init();
+    TEST_ASSERT(retVal == kStatus_MCMGR_Success);
+}
+
+// Test MCMGR_StartCore() function with wrong core number, this core is now running
+void mcmgr_test_start_core1()
+{
+    mcmgr_status_t retVal = MCMGR_StartCore(THIS_CORE, BOOT_ADDRESS, 0, kMCMGR_Start_Asynchronous);
+    TEST_ASSERT(retVal == kStatus_MCMGR_Error);
+}
+
+// Test MCMGR_StartCore() function with invalid core number
+void mcmgr_test_start_core2()
+{
+    mcmgr_status_t retVal =
+        MCMGR_StartCore((mcmgr_core_t)INVALID_CORE_NUMBER, BOOT_ADDRESS, 0, kMCMGR_Start_Asynchronous);
+    TEST_ASSERT(retVal == kStatus_MCMGR_Error);
+}
+
+// Test MCMGR_StartCore function with correct arguments
+// In this test is at TEST_ADDRESS written zero value (uint32), next is copied code
+// for secondary core to BOOT_ADDRESS and secondary core is started by API function.
+// Secondary core should write TEST_VALUE at TEST_ADDRESS and stop in infinite loop,
+// main core wait 100ms for start and executing code of secondary core and reads
+// value from TEST_ADDRESS and test if value is equal to TEST_VALUE
+void mcmgr_test_start_core3()
+{
+    // set 4bytes at TEST_ADDRESS to zero
+    volatile uint32_t *addr = (uint32_t *)TEST_ADDRESS;
+    *addr                   = 0x0;
+
+#ifdef APP_INVALIDATE_CACHE_FOR_SECONDARY_CORE_IMAGE_MEMORY
+    invalidate_cache_for_core1_image_memory((uint32_t)TEST_ADDRESS, 4);
+#endif /* APP_INVALIDATE_CACHE_FOR_SECONDARY_CORE_IMAGE_MEMORY */
+
+#if !(defined(KW45B41Z83_cm33_SERIES) || defined(KW47B42ZB7_cm33_core0_SERIES) || defined(MCXW727C_cm33_core0_SERIES) || defined(K32L3A60_cm4_SERIES))
+    // code for secondary core
+    // writes TEST_VALUE at TEST_ADDRESS, stops in infinite loop
+    //    SP
+    //    PC
+    // ********* Only For RT700 ***********
+    //    MOVS R1 #1         0x2101 - Copy number 1 into R1
+    //    LDR R0, [PC, #12]  0x4803 - Copy address to write Arbitter0 to R0
+    //    STR R1, [R0]       0x6001 - Write R1 value to R0 address set above
+    //    NOP                0xbf00 - NOP instruction to align code to 4 bytes
+    // ************************************
+    //    LDR R0, [PC, #4]   0x4802
+    //    LDR R1, [PC, #8]   0x4903
+    //    STR R1, [R0]       0x6001
+    //    B .                0xe7fe
+    //    NOP                0xbf00
+
+    uint32_t code[] = {
+/* RT1180 CM7 core runs from ITCM with aliased address, SP and PC need to be extra defined */
+#if (defined(MIMXRT1187_cm33_SERIES) || defined(MIMXRT1189_cm33_SERIES))
+        (uint32_t)0x20500000, // SP
+        (uint32_t)0x9,        // PC
+#else
+        (uint32_t)TEST_ADDRESS + 0x100, // SP
+        (uint32_t)BOOT_ADDRESS + 0x9,   // PC
+#endif
+#if (defined(MIMXRT798S_cm33_core0_SERIES))
+        0x48032101,
+        0xbf006001,
+#endif
+        0x49034802, // first two instructions
+        0xe7fe6001, // next two instructions
+#if (defined(MIMXRT798S_cm33_core0_SERIES))
+        CPU1_RAM_ATBITTER0_ADDR,
+#else
+        0xbf00bf00,                      // Filler of NOPs to keep aligment
+#endif
+        TEST_ADDRESS, // test address where second core writes
+        TEST_VALUE    // value second core writes
+    };
+
+    // copy code for secondary core to RAM starting at BOOT_ADDRESS
+    void *secCoreBootAddr = BOOT_ADDRESS;
+    memcpy(secCoreBootAddr, code, sizeof(code));
+
+#ifdef APP_INVALIDATE_CACHE_FOR_SECONDARY_CORE_IMAGE_MEMORY
+    invalidate_cache_for_core1_image_memory((uint32_t)BOOT_ADDRESS, 0x200);
+#endif /* APP_INVALIDATE_CACHE_FOR_SECONDARY_CORE_IMAGE_MEMORY */
+
+#endif
+
+    // start the second core
+    mcmgr_status_t retVal = MCMGR_StartCore(kMCMGR_Core1, BOOT_ADDRESS, 0, kMCMGR_Start_Asynchronous);
+    TEST_ASSERT(retVal == kStatus_MCMGR_Success);
+
+    // wait for start and executing code of secondary core
+    volatile int i;
+    for (i = 0; i < 10000; i++)
+        ;
+
+#ifdef APP_INVALIDATE_CACHE_FOR_SECONDARY_CORE_IMAGE_MEMORY
+    invalidate_cache_for_core1_image_memory((uint32_t)TEST_ADDRESS, 4);
+#endif /* APP_INVALIDATE_CACHE_FOR_SECONDARY_CORE_IMAGE_MEMORY */
+
+    // read uint32 from TEST_ADDRESS
+    volatile uint32_t val = *addr;
+
+    // compare with value TEST_VALUE, which should write secondary code after start
+    TEST_ASSERT(val == TEST_VALUE);
+}
+
+// Test ELE_GetFwStatus() function check if FW ELE is loaded
+#if (defined(MIMXRT1187_cm33_SERIES) || defined(MIMXRT1189_cm33_SERIES))
+void mcmgr_test_get_ele_stat()
+{
+    uint32_t fwstatus = 0xFFFFFFFFu;
+    TEST_ASSERT(ELE_GetFwStatus(S3MU, &fwstatus) == kStatus_Success);
+    TEST_ASSERT(fwstatus == 0u);
+}
+#endif
+
+// Test MCMGR_StopCore() function with wrong core number, this core is now running
+void mcmgr_test_stop_core1()
+{
+    mcmgr_status_t retVal = MCMGR_StopCore(THIS_CORE);
+    TEST_ASSERT(retVal == kStatus_MCMGR_Error);
+}
+
+// Test MCMGR_StopCore() function with invalid core number
+void mcmgr_test_stop_core2()
+{
+    mcmgr_status_t retVal = MCMGR_StopCore((mcmgr_core_t)INVALID_CORE_NUMBER);
+    TEST_ASSERT(retVal == kStatus_MCMGR_Error);
+}
+
+// Test MCMGR_StopCore() function with valid core number
+// The test address is cleared and if the second core is
+// really stopped, it should remain cleared.
+void mcmgr_test_stop_core3()
+{
+#if defined(CPU_MIMXRT1176DVMAA_cm7) || defined(CPU_MIMXRT1166DVM6A_cm7)
+    mcmgr_status_t retVal = MCMGR_StopCore(kMCMGR_Core1);
+    TEST_ASSERT(retVal == kStatus_MCMGR_NotImplemented);
+#else
+    mcmgr_status_t retVal = MCMGR_StopCore(kMCMGR_Core1);
+    TEST_ASSERT(retVal == kStatus_MCMGR_Success);
+
+#if !(defined(KW45B41Z83_cm33_SERIES) || defined(KW47B42ZB7_cm33_core0_SERIES) || defined(MCXW727C_cm33_core0_SERIES))
+    // set 4bytes at TEST_ADDRESS to zero
+    uint32_t *addr = (uint32_t *)TEST_ADDRESS;
+    *addr          = 0x0;
+
+    // wait for start and executing code of secondary core
+    int i;
+    for (i = 0; i < 10000; i++)
+        ;
+
+    // read uint32 from TEST_ADDRESS
+    uint32_t val = *addr;
+
+    // compare with value 0 - the second core should be stopped.
+    TEST_ASSERT(val == 0);
+#endif
+#endif
+}
+
+// Test if return value of MCMGR_GetVersion() API function is equal to MCMGR
+// version enum (kMCMGR_Version)
+void mcmgr_test_get_version()
+{
+    int32_t mcmgrVersion = MCMGR_GetVersion();
+    TEST_ASSERT(mcmgrVersion == kMCMGR_Version);
+}
+
+// Test if return value of MCMGR_MapAddress() API function is equal to CORE_COUNT
+void mcmgr_test_get_core_count()
+{
+    uint32_t numCores = MCMGR_GetCoreCount();
+    TEST_ASSERT(numCores == CORE_COUNT);
+}
+
+// Test if return value of MCMGR_GetCurrentCore() API function is equal to
+// value defined for this core (THIS_CORE)
+void mcmgr_test_get_current_core()
+{
+    mcmgr_core_t currentCore = MCMGR_GetCurrentCore();
+    TEST_ASSERT(currentCore == THIS_CORE);
+}
+
+// Test of MCMGR_GetCoreProperty() API function, test if set correct value size
+// of property kMCMGR_CoreStatus
+void mcmgr_test_get_core_property1()
+{
+    uint32_t val          = 0;
+    uint32_t len          = sizeof(val);
+    mcmgr_status_t retVal = MCMGR_GetCoreProperty(kMCMGR_Core0, kMCMGR_CoreStatus, &val, &len);
+    if (retVal != kStatus_MCMGR_NotImplemented)
+    {
+        TEST_ASSERT(retVal == kStatus_MCMGR_Success);
+        TEST_ASSERT(val == kMCMGR_CoreTypeCortexM0Plus);
+    };
+}
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+/* This function is used for the Corn test automation framework
+   to breakpoint/stop the execution and to capture results
+   from the memory. It must be ensured that it will never be inlined
+   and optimized to allow proper address recognition and breakpoint
+   placement during the Corn execution. */
+__attribute__((noinline)) void CornBreakpointFunc(void)
+{
+    volatile int i = 0;
+    i++;
+}
+
+int main(int argc, char **argv)
+{
+    BOARD_InitHardware();
+
+    PRINTF("\r\n**** MCMGR TESTS ****\r\n");
+
+    UnityBegin();
+#ifdef __COVERAGESCANNER__
+    __coveragescanner_testname("mcmgr_test");
+    __coveragescanner_install("mcmgr_test.csexe");
+#endif /*__COVERAGESCANNER__*/
+    RUN_EXAMPLE(mcmgr_test_init_success, MAKE_UNITY_NUM(k_unity_mcmgr, 0));
+
+    RUN_EXAMPLE(mcmgr_test_start_core1, MAKE_UNITY_NUM(k_unity_mcmgr, 5));
+    RUN_EXAMPLE(mcmgr_test_start_core2, MAKE_UNITY_NUM(k_unity_mcmgr, 6));
+    RUN_EXAMPLE(mcmgr_test_start_core3, MAKE_UNITY_NUM(k_unity_mcmgr, 7));
+
+#if (defined(MIMXRT1187_cm33_SERIES) || defined(MIMXRT1189_cm33_SERIES))
+    RUN_EXAMPLE(mcmgr_test_get_ele_stat, MAKE_UNITY_NUM(k_unity_mcmgr, 8));
+#endif /* (defined(defined(MIMXRT1187_cm33_SERIES) ||  defined(MIMXRT1189_cm33_SERIES)) */
+
+#if !(defined(CPU_MIMXRT1176DVMAA_cm7) || defined(CPU_MIMXRT1166DVM6A_cm7) || defined(MIMXRT1187_cm33_SERIES) || \
+      defined(MIMXRT1189_cm33_SERIES) || defined(MIMXRT798S_cm33_core0_SERIES))
+    RUN_EXAMPLE(mcmgr_test_stop_core1, MAKE_UNITY_NUM(k_unity_mcmgr, 9));
+    RUN_EXAMPLE(mcmgr_test_stop_core2, MAKE_UNITY_NUM(k_unity_mcmgr, 10));
+#endif /* !(defined(CPU_MIMXRT1176DVMAA_cm7) || defined(CPU_MIMXRT1166DVM6A_cm7) || defined(MIMXRT1187_cm33_SERIES) || \
+          defined(MIMXRT1189_cm33_SERIES) || defined(MIMXRT798S_cm33_core0_SERIES)) */
+#if !(defined(MIMXRT1187_cm33_SERIES) || defined(MIMXRT1189_cm33_SERIES) || defined(MIMXRT798S_cm33_core0_SERIES))
+    RUN_EXAMPLE(mcmgr_test_stop_core3, MAKE_UNITY_NUM(k_unity_mcmgr, 11));
+#endif /* !(defined(MIMXRT1187_cm33_SERIES) ||  defined(MIMXRT1189_cm33_SERIES) || \
+          defined(MIMXRT798S_cm33_core0_SERIES)) */
+
+    RUN_EXAMPLE(mcmgr_test_get_version, MAKE_UNITY_NUM(k_unity_mcmgr, 15));
+    RUN_EXAMPLE(mcmgr_test_get_core_count, MAKE_UNITY_NUM(k_unity_mcmgr, 19));
+    RUN_EXAMPLE(mcmgr_test_get_current_core, MAKE_UNITY_NUM(k_unity_mcmgr, 20));
+    RUN_EXAMPLE(mcmgr_test_get_core_property1, MAKE_UNITY_NUM(k_unity_mcmgr, 21));
+
+    UnityEnd();
+    CornBreakpointFunc();
+
+    while (1)
+    {
+    };
+}
---
+++ tests/test_sync_start/secondary/main.c
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2016-2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "unity.h"
+#include "fsl_device_registers.h"
+#include "fsl_common.h"
+//#include "board.h"
+#include "mcmgr.h"
+#include "app.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define TEST_VALUE 0xAAAAAAAA
+#define TEST_VALUE_16B 0xBBBB
+#define HARDFAULT_TRIGGER_EVENT (5)
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+static volatile int appEvent = 0;
+void MCMGR_RemoteApplicationEventHandler(uint16_t remoteData, void *context)
+{
+   int* appEvent = (int*)context;
+   *appEvent = remoteData;
+}
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void mcmgr_test_init_success_sec_core()
+{
+    uint32_t startupData;
+    mcmgr_status_t status = kStatus_MCMGR_Error;
+
+   /* Initialize MCMGR before calling its API */
+    status = MCMGR_Init();
+    TEST_ASSERT(status == kStatus_MCMGR_Success);
+
+    /* Install remote application event */
+    status = MCMGR_RegisterEvent(kMCMGR_RemoteApplicationEvent, MCMGR_RemoteApplicationEventHandler, (void*)&appEvent);
+    TEST_ASSERT(status == kStatus_MCMGR_Success);
+
+    /* Get the startup data */
+    do{
+        status = MCMGR_GetStartupData(&startupData);
+    }while(status != kStatus_MCMGR_Success);
+
+   
+    // wait here, to make sure it is not a coincidence
+    // the master core manages to read the TEST_VALUE.
+    int i;
+    for (i = 0; i < 10000; i++)
+        ;
+
+
+    // write the test value
+    ((uint32_t *)startupData)[0] = TEST_VALUE;
+
+    // Now, start function on master can return
+    // pass the TEST_VALUE_16B to the other side in the callback 
+    status = MCMGR_TriggerEvent(kMCMGR_RemoteApplicationEvent, TEST_VALUE_16B);
+    TEST_ASSERT(status == kStatus_MCMGR_Success);
+}
+
+void mcmgr_test_bad_args_sec_core()
+{
+    mcmgr_status_t retVal = kStatus_MCMGR_Success;
+    /* Testing bad args, to be added into unity */ 
+    retVal = MCMGR_GetStartupData(NULL);
+    TEST_ASSERT(retVal == kStatus_MCMGR_Error);
+}
+
+void run_tests(void *unused)
+{
+#ifdef __COVERAGESCANNER__
+    __coveragescanner_testname("mcmgr_sync_start_test_sec_core");
+    __coveragescanner_install("mcmgr_sync_start_test_sec_core.csexe");
+#endif /*__COVERAGESCANNER__*/
+    RUN_EXAMPLE(mcmgr_test_init_success_sec_core, MAKE_UNITY_NUM(k_unity_mcmgr, 0));
+    RUN_EXAMPLE(mcmgr_test_bad_args_sec_core, MAKE_UNITY_NUM(k_unity_mcmgr, 1));
+}
+
+int main(void)
+{
+    int i = 0;
+    
+    BOARD_InitHardware();
+
+    UnityBegin();
+    run_tests(NULL);
+    UnityEnd();
+  
+    // wait until the primary core notifies hardfault can be triggered
+    while(HARDFAULT_TRIGGER_EVENT != appEvent);
+
+    // Now, trigger an exception here! Try to write to flash. 
+    *((uint32_t*)0xFFFFFFFF) = i;
+
+    while (1)
+        ;
+}
